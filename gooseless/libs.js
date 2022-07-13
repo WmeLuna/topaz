bb67273eb138c6db77bfa3ce7823249311cbd1ed
WmeLuna <warriorallen11@gmail.com>
2022-07-13 01:32:05 -0400

    gooseless

--- gooseless/libs.js
+++ gooseless/libs.js
@@ -12,7 +12,8 @@ Object.defineProperty(window, 'localStorage', getLocalStoragePropertyDescriptor(
 //webpack add
 const gooseWebPack = document.createElement('script')
 gooseWebPack.textContent = `
-    window.goosemod = {webpackModules:{},patcher:{},settings:{}};
+    window.goosemod = {webpackModules:{},patcher:{},settings:{},reactUtils:{}};
     import('https://raw.githack.com/GooseMod/GooseMod/master/src/util/discord/webpackModules.js').then(m => window.goosemod.webpackModules = m)
+    import('https://raw.githack.com/GooseMod/GooseMod/master/src/util/react.js').then(m => window.goosemod.reactUtils = m)
 `
 document.head.appendChild(gooseWebPack);
