50662e73fe27f4eb1fb1ba9f97a320115dc951b0
WmeLuna <warriorallen11@gmail.com>
2022-07-13 02:26:24 -0400

    fix

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-eval(await (await fetch('https://wmeluna.com/topaz/gooseless/libs.js')).text())
+(async () => {
+    eval(await (await fetch('https://wmeluna.com/topaz/gooseless/libs.js')).text())
 eval(await (await fetch('https://wmeluna.com/topaz/gooseless/goosesettings.js')).text())
 eval(await (await fetch('https://wmeluna.com/topaz/gooseless/goosepatcher.js')).text())
 eval(await (await fetch('https://wmeluna.com/topaz/src/index.js')).text())
+})
